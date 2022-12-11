0cc2cff0a5183247decc0fa5db9df7655682102e
Nam Nguyen <meomaythongminh3@gmail.com>
2022-12-12 01:29:07 +0700

    Change stack navbar

--- App.js
+++ App.js
@@ -1,29 +1,48 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Home from './screens/Home';
-import Scanner from './screens/Scanner';
+import Home from "./screens/Home";
+import Scanner from "./screens/Scanner";
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 const Stack = createNativeStackNavigator();
+const MyTheme = {
+  colors: {
+    primary: "rgb(82, 101, 255)",
+    backgroundColor: "rgb(82, 101, 255)",
+  },
+};
 
-function  App(){
-  return(
+function App() {
+  return (
     <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Scanner" component={Scanner} />
+      <Stack.Screen name="Home" component={Home} />
+      {/* <Stack.Screen name="Scanner" component={Scanner} /> */}
+      <Stack.Screen
+        name="Scanner"
+        component={Scanner}
+        options={{
+          headerTintColor: "white",
+          headerStyle: { 
+            backgroundColor: "#5265FF", 
+            // justifyContent: 'center',
+            // alignItems: 'center',
+            // textAlign: 'center'
+          },
+          headerTitleAlign: 'center'
+        }}
+      />
     </Stack.Navigator>
-  )
+  );
 }
-
 
 export default () => {
   return (
-    <NavigationContainer>
-          <App/>
+    <NavigationContainer barStyle={{ backgroundColor: "#694fad" }}>
+      <App />
     </NavigationContainer>
   );
-} 
+};
---
+++ screens/Bill.js
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+
+export default function Bill(){
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container :{
+      flex : 1,
+      alignItems : 'center',
+      justifyContent :'center'
+  }
+})
